9332dacc5fc0d9171bfa35cbd6edd9dc782be3ca
rparrish <rparrish@flightweb.com>
2016-06-12 18:15:14 -0700

    added error text if < 6 data points

--- functions.R
+++ functions.R
@@ -80,6 +80,7 @@ qic_plot <- function(metric_name = "Pediatric Capnography",
     qd <- qic_data(metric_name, program_name = program_name) 
     names(qd) <- c("program_name", "month", "y", "n", "metric") 
     
+    if(nrow(qd) > 6) {
     plot_result <- 
         qic(
         y = y, #unintended_hypothermia, 
@@ -98,15 +99,21 @@ qic_plot <- function(metric_name = "Pediatric Capnography",
         #ylim = c(0,100),
         cex = 1.0,
         las = 2,
-        nint = 12,
-        freeze = 12,
-        print = FALSE
-        #plot = TRUE 
+        nint = 3,
+        #freeze = 12,
+        print.out = TRUE,
+        plot.chart = TRUE 
         #runvals = TRUE
         #sub = "subtitle"
         
-        )
-    
+        ) } else {
+           #  http://stackoverflow.com/questions/19918985/r-plot-only-text
+            plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+            text(x = 0.5, y = 0.5, paste("Insufficient data"), 
+                 cex = 1.6, col = "black")
+            
+        }
+   
     results <- list(plot_result = plot_result, data = qd)
     invisible(results)
     
